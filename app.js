6b9496763e988dafb2331c296f5eb8fb236b4f59
unknown <mrzar1380@gmail.com>
2023-12-01 23:56:42 +0330

    struc

--- app.js
+++ app.js
@@ -7,16 +7,28 @@ const token = "6555410778:AAFrseXgj6HGWtvkv2qhmovPqIadaJAyMFE";
 const bot = new TelegramBot(token, { polling: true });
 
 // Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
-
+bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
 
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
+  // Define the keyboard layout
+  const keyboard = [
+    [
+      { text: "Button 1", callback_data: "button1" },
+      { text: "Button 2", callback_data: "button2" },
+    ],
+    [
+      { text: "Button 3", callback_data: "button3" },
+      { text: "Button 4", callback_data: "button4" },
+    ],
+  ];
+
+  // Create the inline keyboard
+  const replyMarkup = {
+    inline_keyboard: keyboard,
+  };
+
+  // Send a message with the inline keyboard
+  bot.sendMessage(chatId, "Choose an option:", { reply_markup: replyMarkup });
 });
 
 // Listen for any kind of message. There are different kinds of
